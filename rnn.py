5dd7ff16461a62a834a6cf9674cb70ab1795fb5d
DaraMoh <dxm190032@utdallas.edu>
2024-11-15 20:43:53 -0600

    Finished ffnn.py and rnn.py

--- rnn.py
+++ rnn.py
@@ -17,26 +17,33 @@ unk = '<UNK>'
 # Consult the PyTorch documentation for information on the functions used below:
 # https://pytorch.org/docs/stable/torch.html
 class RNN(nn.Module):
-    def __init__(self, input_dim, h):  # Add relevant parameters
+    def __init__(self, input_dim, h):  # input_dim: size of word embeddings, h: size of hidden state
         super(RNN, self).__init__()
         self.h = h
         self.numOfLayer = 1
-        self.rnn = nn.RNN(input_dim, h, self.numOfLayer, nonlinearity='tanh')
-        self.W = nn.Linear(h, 5)
+        self.rnn = nn.RNN(input_dim, h, self.numOfLayer, nonlinearity='tanh', batch_first=True)
+        self.W = nn.Linear(h, 5)  # Output layer (5 classes, adjust this for your problem)
         self.softmax = nn.LogSoftmax(dim=1)
-        self.loss = nn.NLLLoss()
+        self.loss = nn.NLLLoss()  # Negative log-likelihood loss for classification
 
     def compute_Loss(self, predicted_vector, gold_label):
+        # Compute loss for the output of the RNN model
         return self.loss(predicted_vector, gold_label)
 
     def forward(self, inputs):
-        # [to fill] obtain hidden layer representation (https://pytorch.org/docs/stable/generated/torch.nn.RNN.html)
-        _, hidden = 
-        # [to fill] obtain output layer representations
+        # inputs: shape (batch_size, seq_length, embedding_dim)
 
-        # [to fill] sum over output 
+        # Pass the inputs through the RNN
+        rnn_out, hidden = self.rnn(inputs)  # rnn_out shape: (batch_size, seq_length, hidden_dim)
 
-        # [to fill] obtain probability dist.
+        # Only take the output of the last time step (for sequence classification)
+        last_output = rnn_out[:, -1, :]  # shape: (batch_size, hidden_dim)
+
+        # Pass through the output layer
+        output = self.W(last_output)  # shape: (batch_size, num_classes)
+
+        # Apply LogSoftmax to get probabilities
+        predicted_vector = self.softmax(output)
 
         return predicted_vector
 
@@ -80,7 +87,7 @@ if __name__ == "__main__":
     model = RNN(50, args.hidden_dim)  # Fill in parameters
     # optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
     optimizer = optim.Adam(model.parameters(), lr=0.01)
-    word_embedding = pickle.load(open('./word_embedding.pkl', 'rb'))
+    word_embedding = pickle.load(open('Data_Embedding/word_embedding.pkl', 'rb'))
 
     stopping_condition = False
     epoch = 0
